ad5bfe66c8062ea47668b6483e857ba325317f7c
AlexKaroh <steam.alexkaroh@gmail.com>
2023-05-15 17:37:12 +0300

    feat: implement get cell position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit  {
+  @ViewChildren('square') square: QueryList<ElementRef> | undefined;
 
   title = '5x5';
-
-  @ViewChild('field') gameField: ElementRef | undefined;
 
   field = [
     [0,0,0,0,0],
@@ -20,5 +19,18 @@ export class AppComponent {
   ];
 
   constructor() {}
+
+  ngAfterViewInit(): void {}
+
+  getPosition(event: any) {
+    const elementsArr = this.square?.toArray().map(el => el.nativeElement);
+    const clickedIndex = elementsArr!.indexOf(event);
+    const rowSize = 5;
+    let col = clickedIndex % rowSize;
+    let row = Math.floor(clickedIndex / rowSize);
+    console.log(`row: ${++row}, col: ${++col}`);
+  }
+
+  ngOnInit(): void {}
 
 }
