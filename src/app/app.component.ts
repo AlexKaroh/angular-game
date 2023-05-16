4ab41d56df65e8fcad33b609ecbbcf1a24da3ca6
AlexKaroh <steam.alexkaroh@gmail.com>
2023-05-16 14:11:53 +0300

    feat: add restart game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,38 @@ export class AppComponent implements OnInit  {
   @ViewChildren('square') square: QueryList<ElementRef> | undefined;
   title = '5x5';
   field = Array(25).fill(0);
+  isFirstMove = true;
 
 
   constructor() {}
 
-  getPosition(event: EventTarget| null) {
-    this.clearUnusedCells();
-    const elementsArr = this.square!.toArray().map(el => el.nativeElement);
-    const clickedIndex = elementsArr.indexOf(event);
-    this.getPossibleMoves(clickedIndex);
-    this.field[clickedIndex] = 1;
+  restartGame() {
+    this.field = Array(25).fill(0);
+    this.isFirstMove = true;
   }
 
-  clearUnusedCells(): void {
+  getPosition(event: EventTarget| null) {
+    const elementsArr = this.square!.toArray().map(el => el.nativeElement);
+    const clickedIndex = elementsArr.indexOf(event);
+
+    if(this.isFirstMove === true && this.field[clickedIndex] === 0) {
+      this.confirmMove(clickedIndex);
+      this.isFirstMove = false;
+    }
+
+    if(this.isFirstMove === false && this.field[clickedIndex] === 2) {
+      this.confirmMove(clickedIndex);
+    }
+  }
+
+  clearUnusedCells() {
     for (let i = 0; i < this.field.length; i++){
       if(this.field[i] === 2) this.field[i] = 0;
       if(this.field[i] === 1) this.field[i] = 3;
     }
   }
 
-  getPossibleMoves(index: number): void {
+  getPossibleMoves(index: number) {
     const rowSize = 5;
     const row = Math.floor(index / rowSize);
     const col = index % rowSize;
@@ -55,10 +67,14 @@ export class AppComponent implements OnInit  {
     });
   }
 
+  confirmMove(clickedIndex: number) {
+    this.clearUnusedCells();
+    this.getPossibleMoves(clickedIndex);
+    this.field[clickedIndex] = 1;
+  }
+
   getCellClass(cell: number): string {
     switch (cell) {
-      case 0:
-        return 'empty';
       case 1:
         return 'active';
       case 2:
@@ -66,7 +82,7 @@ export class AppComponent implements OnInit  {
       case 3:
         return 'inactive'
       default:
-        return '';
+        return 'empty';
     }
   }
 
